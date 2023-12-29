8039138c8d5b0a9c9daa4ce9eccdad02a4633f7c
Martignago <hiudy10@gmail.com>
2023-12-29 15:13:03 -0300

    novo

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,38 @@
 // pages/index.js
 
-import fs from 'fs';
-
-export default function Home({ places }) {
-  return (
-    <div>
-      <h1>Locais (coordenadas no brazil):</h1>
-      <ul>
-        {places.map((place, index) => (
-          <li key={index}>
-            {place.nome} - {place.latitude}, {place.longitude}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export async function getServerSideProps() {
-  let places = [];
-
-  try {
-    const data = fs.readFileSync('places.json', 'utf8');
-    places = JSON.parse(data);
-    console.log('Conteúdo do arquivo places.json:', places);
-  } catch (error) {
-    console.error('Erro ao ler o JSON do arquivo places.json:', error);
+export default function Home() {
+    const places = [
+      {
+        "nome": "Curitiba",
+        "latitude": 40.7128,
+        "longitude": -74.0060
+      },
+      {
+        "nome": "Sao Paulo",
+        "latitude": 34.0522,
+        "longitude": -118.2437
+      },
+      {
+        "nome": "Florianopolis",
+        "latitude": 51.5074,
+        "longitude": -0.1278
+      }
+    ];
+  
+    return (
+      <div>
+        <h1>Locais (coordenadas no Brasil):</h1>
+        <ul>
+          {places.map((place, index) => (
+            <li key={index}>
+              {place.nome} - {place.latitude}, {place.longitude}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
-
-  return {
-    props: {
-      places,
-    },
-  };
-}
+  
 
 
 
