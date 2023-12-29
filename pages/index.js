d87a98d70a5ba889b4501dfbc0d403b15a5a4394
Martignago <hiudy10@gmail.com>
2023-12-29 17:10:27 -0300

    novo atualiza

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-// Importar os dados do arquivo JSON
-const placesData = require('../places.json');
+// pages/api/index.js
 
-// Definir a função que será executada quando o endpoint for acessado
-module.exports = (req, res) => {
-  // Retornar os dados como resposta à requisição
+import placesData from '../../places.json'; // Importa os dados do arquivo JSON
+
+export default function handler(req, res) {
+  // Retorna os dados do arquivo JSON como resposta à requisição
   res.status(200).json(placesData);
-};
+}
+
 
   
 
