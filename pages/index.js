e7acb6d787963096d2ad9d174cf5ab81c1cc5a2a
Martignago <hiudy10@gmail.com>
2023-12-29 17:05:17 -0300

    novo atualizacao

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,12 @@
-// pages/index.js
+// Importar os dados do arquivo JSON
+const placesData = require('../places.json');
 
-export default function Home() {
-    const places = [
-      {
-        "id": "1",
-        "nome": "Curitiba",
-        "latitude": 40.7128,
-        "longitude": -74.0060
-      },
-      {
-        "id": "2",
-        "nome": "Sao Paulo",
-        "latitude": 34.0522,
-        "longitude": -118.2437
-      },
-      {
-        "id": "3",
-        "nome": "Florianopolis",
-        "latitude": 51.5074,
-        "longitude": -0.1278
-      },
-      {
-        "id": "4",
-        "nome": "Maceio",
-        "latitude": 51.5930,
-        "longitude": -0.1946
-      }
-    ];
-  
-    return (
-      <div>
-        <h1>Locais (coordenadas no Brasil):</h1>
-        <ul>
-          {places.map((place, index) => (
-            <li key={index}>
-              {place.id} = {place.nome} - {place.latitude}, {place.longitude}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+// Definir a função que será executada quando o endpoint for acessado
+module.exports = (req, res) => {
+  // Retornar os dados como resposta à requisição
+  res.status(200).json(placesData);
+};
+
   
 
 
