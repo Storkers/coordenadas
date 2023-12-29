ac39718fbf2c4c7b6c01858594c0e53d557350a8
Martignago <hiudy10@gmail.com>
2023-12-29 15:16:17 -0300

    novo atualizacao

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,28 @@
 export default function Home() {
     const places = [
       {
+        "id": "1",
         "nome": "Curitiba",
         "latitude": 40.7128,
         "longitude": -74.0060
       },
       {
+        "id": "2",
         "nome": "Sao Paulo",
         "latitude": 34.0522,
         "longitude": -118.2437
       },
       {
+        "id": "3",
         "nome": "Florianopolis",
         "latitude": 51.5074,
         "longitude": -0.1278
+      },
+      {
+        "id": "4",
+        "nome": "Maceio",
+        "latitude": 51.5930,
+        "longitude": -0.1946
       }
     ];
   
@@ -25,7 +34,7 @@ export default function Home() {
         <ul>
           {places.map((place, index) => (
             <li key={index}>
-              {place.nome} - {place.latitude}, {place.longitude}
+              {place.id} = {place.nome} - {place.latitude}, {place.longitude}
             </li>
           ))}
         </ul>
